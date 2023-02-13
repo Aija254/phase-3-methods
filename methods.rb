69db839fdccf98089da2a7b23dbe2b10aa2020ea
EUGENE AIJA <eugeneosu7@gmail.com>
2023-02-13 09:58:13 +0300

    Completed

--- methods.rb
+++ methods.rb
@@ -1,1 +1,56 @@
 # Your code here!
+#1
+def greet_programmer 
+
+    print "Hello, programmer!"
+
+end
+
+#2
+
+def greet name
+    print "Hello, #{name}!"
+
+end
+
+#3
+
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+
+end
+
+
+
+#4
+
+
+def add num1, num2
+    num1 + num2 
+end
+
+
+
+def halve number
+    if number.class != Integer
+        return nil
+    else
+        return number /2
+        
+    end
+end
+#5
+# function halve(number) {
+#     if (typeof number !== "number") return null;
+  
+#     return number / 2;
+#   }
+
+# def halve(number)
+#     if number.class = "integer"
+#         return number / 2
+#     else
+#         put "null"
+
+# end
